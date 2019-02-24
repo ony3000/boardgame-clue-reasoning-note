802e0d42bf49ed6b39128f1059dc16b89f8193dc
Hyeonjong <nianelo4@gmail.com>
2019-02-24 15:26:30 +0900

    Remove participant setting dialog temporarily

--- src/note-app.vue
+++ src/note-app.vue
@@ -4,7 +4,7 @@
         <v-toolbar app>
             <newgame-dialog></newgame-dialog>
             <v-spacer></v-spacer>
-            <player-dialog></player-dialog>
+            <!-- <player-dialog></player-dialog> -->
             <brush-dialog></brush-dialog>
         </v-toolbar>
         <v-content>
@@ -30,7 +30,7 @@
 
 <script>
 import NewgameDialog from '@/components/newgame-dialog';
-import PlayerDialog from '@/components/player-dialog';
+// import PlayerDialog from '@/components/player-dialog';
 import BrushDialog from '@/components/brush-dialog';
 import DetectiveList from '@/components/detective-list';
 import EvidenceNote from '@/components/evidence-note';
@@ -39,7 +39,7 @@ export default {
     name: 'note-app',
     components: {
         NewgameDialog,
-        PlayerDialog,
+        // PlayerDialog,
         BrushDialog,
         DetectiveList,
         EvidenceNote,
